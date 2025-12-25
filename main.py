949767e839766e22bc3387d4488ee3c8a397c450
kimyongbin <dragonbin05@gmail.com>
2025-12-25 17:23:10 +0900

    edit main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
-st.write("# :red[KNN] Practice")
+st.set_page_config(
+    page_title="KNN Practice",
+    page_icon="🤖"
+)
 
+st.write("# :red[KNN] Practice")
+st.write("---")
 st.write("#### 1. KNN with :blue[Iris Dataset]")
